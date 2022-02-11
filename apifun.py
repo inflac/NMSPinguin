073930e0f23e4d220774fb5eb9fd3b02609601ad
inflac <inflac@inflacsan.de>
2022-02-11 20:45:50 +0100

    Initial project version

---
+++ apifun.py
@@ -0,0 +1,110 @@
+from turtle import up
+import requests
+
+#Age guesser
+def get_age(name):
+    url = f'https://api.agify.io/?name=' 
+    data = requests.get(url +name).json()
+    return str(data['age'])
+
+#Math Facts
+def get_new_mathfact():
+    url = f'http://numbersapi.com/random/math'
+    data = requests.get(url).text
+    return data
+
+#Chuch Norris Facts
+def get_new_chuckfact():
+    url = f'https://api.chucknorris.io/jokes/random'
+    data = requests.get(url).json()
+    return data['value']
+
+#Cat Facts
+def get_new_catfact():
+    url = f'https://catfact.ninja/fact'
+    data = requests.get(url).json()
+    return data['fact']
+
+#Cat Pics
+def get_new_catpic():
+    url = f'https://cataas.com/cat?json=true'
+    data = requests.get(url).json()
+    return "https://cataas.com" + data['url'] 
+
+#Dog Pics
+def get_new_dogpic():
+    url = f'https://dog.ceo/api/breeds/image/random'
+    data = requests.get(url).json()
+    return data['message']
+
+#Nasa Pics
+def get_new_nasapic():
+    url = f'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+    data = requests.get(url).json()
+    return data['url']
+
+#Corona Inzidenz
+def corona_updater():
+    url = f'https://api.corona-zahlen.org/districts/05911'
+    data = requests.get(url).json()
+    return data
+
+class corona_update:
+    def __init__(self, data):
+        self.cases = data['data']['05911']['cases']
+        self.deaths = data['data']['05911']['deaths']
+        self.casesperweek = data['data']['05911']['casesPerWeek']
+        self.deathsperweek = data['data']['05911']['deathsPerWeek']
+        self.recovered = data['data']['05911']['recovered']
+        self.weekincidence = data['data']['05911']['weekIncidence']
+        self.source = data['meta']['source']
+
+    def get_cases(self):
+        return self.cases
+    def get_deaths(self):
+        return self.deaths
+    def get_casesperweek(self):
+        return self.casesperweek
+    def get_deathsperweek(self):
+        return self.deathsperweek
+    def get_recovered(self):
+        return self.recovered
+    def get_weekincidence(self):
+        return self.weekincidence
+    def get_source(self):
+        return self.source
+    def get_corona_update(self):
+        return  "# Bislang erkrankter Personen: " + str(self.cases) + \
+                "\n# Bislang verstorbener Personen: " + str(self.deaths) + \
+                "\n# Erkrankter in dieser Woche: " + str(self.casesperweek) + \
+                "\n# Verstorbener in dieser Woche: " + str(self.deathsperweek) + \
+                "\n# Genesener Personen: " + str(self.recovered) + \
+                "\nWöchentliche Inzidenz: " + str(self.weekincidence) + \
+                "\nDieser Bot nutzt die API von: " + str(self.source)
+
+#Jokes
+def get_new_joke():
+    url = f'https://v2.jokeapi.dev/joke/Programming?lang=de&blacklistFlags=nsfw,racist,sexist,explicit'
+    data = requests.get(url).json()
+    return data
+
+class joker:
+    def __init__(self, data):
+        self.type = data['type']
+        if self.type == "single":
+            self.joke = data['joke']
+            self.setup = False
+            self.delivery = False
+        else:
+            self.joke = False
+            self.setup = data['setup'] 
+            self.delivery = data['delivery']
+
+    def get_joke_type(self):
+        return self.type
+    def get_single(self):
+        return self.joke
+    def get_setup(self):
+        return self.setup
+    def get_delivery(self):
+        return self.delivery
---
+++ main.py
@@ -0,0 +1,82 @@
+from asyncio import wait_for
+import asyncio
+from asyncio.windows_events import NULL
+from importlib.metadata import files
+from logging import NullHandler
+from pyexpat.errors import messages
+import discord
+import apifun as apifun
+from secret import token as TOKEN
+
+
+class MyClient(discord.Client):
+    #Einloggen
+    async def on_ready(self):
+        print("Ich habe mich eingeloggt Beep Boop")
+    
+    #Wenn Nachricht gepostet wird
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+    #Inzidenz
+        if message.content.lower() == "!corona":
+            update = apifun.corona_update(apifun.corona_updater())
+            await message.channel.send(update.get_corona_update())
+
+    #Jokes
+        if message.content.lower() == "!joke":
+            joke = apifun.joker(apifun.get_new_joke())
+            if joke.get_joke_type() == "single":
+                await message.channel.send(joke.get_single())
+            else:
+                await message.channel.send(joke.get_setup())
+                await message.channel.send(joke.get_delivery())
+
+    #CatFact
+        if message.content.lower() == "!catfact":
+            await message.channel.send(apifun.get_new_catfact())
+
+    #MathFact
+        if message.content.lower() == "!mathfact":
+            await message.channel.send(apifun.get_new_mathfact())
+
+    #Nasapic
+        if message.content.lower() == "!nasapic":
+            await message.channel.send(apifun.get_new_nasapic())
+
+    #Dogpic
+        if message.content.lower() == "!dog":
+            await message.channel.send(apifun.get_new_dogpic())
+        #Dogbomb
+        if message.content.lower() == "!dogbomb":
+            for i in range(5):
+                await message.channel.send(apifun.get_new_dogpic())
+
+    #Dogpic
+        if message.content.lower() == "!cat":
+            await message.channel.send(apifun.get_new_catpic())
+        #Dogbomb
+        if message.content.lower() == "!catbomb":
+            for i in range(5):
+                await message.channel.send(apifun.get_new_catpic())
+
+    #ChucknorisFact
+        if message.content.lower() == "!chuckfact":
+            await message.channel.send(apifun.get_new_chuckfact())
+
+    #Ageguesser
+
+client = MyClient()
+# @client.event
+# async def on_message(message):
+#     if message.content.lower() == "!age":
+#         channel = message.channel
+#         await channel.send('Schreib mir einen Namen und ich sage dir wie alt er klingt')
+
+#         def check(m):
+#             return m.author == message.author and m.channel == channel
+
+#         msg = await client.wait_for('message', check=check)
+#         await channel.send("Der Name " + str(msg.content) + " klingt für mich als wärst du " + get_age(msg.content) + " Jahre alt.")
+
+client.run(TOKEN)
